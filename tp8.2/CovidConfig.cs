5372e8d5c25fa3516e6a5cebd73b3894163fe4b6
Deavanzack <daffafadillah564@gmail.com>
2024-04-15 02:22:38 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tp8.2/CovidConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.Json;
+
+public class CovidConfig
+{
+    public string suhu { get; set; }
+    public int hari { get; set; }
+    public string pesan_ditolak { get; set; }
+    public string pesan_diterima { get; set; }
+
+    public class suhuconfig
+    {
+        public CovidConfig config;
+        public const string filepath = "C:\\Users\\fadillah\\OneDrive - Telkom University\\Dokumen\\My File\\RPL\\SEMESTER 4\\Kontruksi Perangkat Lunak\\tp8.2\\tp8.2\\covid_config.json";
+        public suhuconfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+
+        public void ReadConfigFile()
+        {
+            string configjsondata = File.ReadAllText(filepath);
+            this.config = JsonSerializer.Deserialize<CovidConfig>(configjsondata);
+
+        }
+        public void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            String jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filepath, jsonString);
+        }
+        private void SetDefault()
+        {
+            config = new CovidConfig();
+            config.suhu = "celcius";
+            config.hari = 14;
+            config.pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+            config.pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+        }
+
+        public void ubahsatuan()
+        {
+            config = new CovidConfig();
+            if (this.config.suhu == "celcius")
+            {
+                this.config.suhu = "farenheit";
+            }
+            else if (config.suhu == "farenheit")
+            {
+                this.config.suhu = "celcius";
+            }
+        }
+    }
+}
---
+++ tp8.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        CovidConfig.suhuconfig cs = new CovidConfig.suhuconfig();
+
+        Console.Write("Berapa suhu badan anda saat ini? Dalam nilai celcius : ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? : ");
+        int demam = int.Parse(Console.ReadLine());
+
+
+        if (((cs.config.suhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) ||
+            (cs.config.suhu == "fahrenheit" && suhu >= 97.7 && suhu <= 99.5)) &&
+            demam < cs.config.hari)
+        {
+            Console.WriteLine(cs.config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(cs.config.pesan_ditolak);
+        }
+
+        cs.ubahsatuan();
+    }
+}
